8dba6efdd86c7cee441ba1db12b42631e41b2a06
Number869 <52751853+Number869@users.noreply.github.com>
2023-12-06 20:41:00 +0100

    DecomposeNavController: - reverted some changes related to current destination tracking due to redundancy

--- core/src/commonMain/kotlin/com/number869/decomposeSimplifications/core/common/DecomposeNavController.kt
+++ core/src/commonMain/kotlin/com/number869/decomposeSimplifications/core/common/DecomposeNavController.kt
@@ -71,17 +71,17 @@ class DecomposeNavController<C : Any>(
 
     fun onBackClicked(toIndex: Int) {
         navigation.popTo(index = toIndex)
-        _currentDestination = stack.items[toIndex].configuration
+        _currentDestination = stack.active.configuration
     }
 
     fun navigate(targetDestination: C) {
         navigation.bringToFront(targetDestination)
-        _currentDestination = targetDestination
+        _currentDestination = stack.active.configuration
     }
 
     fun pop(onComplete: (isSuccess: Boolean) -> Unit = {}) {
-        _currentDestination = stack.items[stack.items.lastIndex - 1].configuration
         navigation.pop(onComplete)
+        _currentDestination = stack.active.configuration
     }
 }
 
