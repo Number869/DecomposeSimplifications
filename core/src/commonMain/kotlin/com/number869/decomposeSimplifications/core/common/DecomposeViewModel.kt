a987451ef18da4c2de10c18eb34c6a811c4bd216
Number869 <52751853+Number869@users.noreply.github.com>
2023-12-06 20:18:56 +0100

    DecomposeViewModel: - decomposeViewModel() functions now have a key parameter

--- core/src/commonMain/kotlin/com/number869/decomposeSimplifications/core/common/DecomposeViewModel.kt
+++ core/src/commonMain/kotlin/com/number869/decomposeSimplifications/core/common/DecomposeViewModel.kt
@@ -8,15 +8,27 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 
-inline fun <reified T: DecomposeViewModel> ComponentContext.decomposeViewModel(viewModel: T) = this.instanceKeeper.getOrCreate { viewModel }
+inline fun <reified T: DecomposeViewModel> ComponentContext.decomposeViewModel(
+    viewModel: T,
+    key: Any? = null
+) = if (key == null) {
+    this.instanceKeeper.getOrCreate { viewModel }
+} else {
+    this.instanceKeeper.getOrCreate(key) { viewModel }
+}
 
 @Composable
 inline fun <reified T: DecomposeViewModel> decomposeViewModel(
     viewModel: T,
+    key: Any? = null,
     componentContext: ComponentContext = checkNotNull(LocalDecomposeComponentContext.current) {
         "No ComponentContext was provided via LocalDecomposeComponentContext"
     }
-) = componentContext.instanceKeeper.getOrCreate { viewModel }
+) = if (key == null) {
+    componentContext.instanceKeeper.getOrCreate { viewModel }
+} else {
+    componentContext.instanceKeeper.getOrCreate(key) { viewModel }
+}
 
 /**
  * Basic view model that is similar to the one that's offered by google for android
